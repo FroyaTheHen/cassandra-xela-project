3cacebdbfca3f6b5744252406b3a4a2ebf83dfc0
FroyaTheHen <kingacepielik@gmail.com>
2024-11-14 11:28:44 -0600

    initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
+
+
+auth_provider = PlainTextAuthProvider(username="cassandra", password="cassandra")
+cluster = Cluster(auth_provider=auth_provider, protocol_version=3, contact_points=['0.0.0.0'], port=9042)
+session = cluster.connect()
+q = """CREATE KEYSPACE IF NOT EXISTS xela_keyspace
+WITH replication = {
+    'class': 'SimpleStrategy', 
+    'replication_factor': 1
+};"""
+session.execute(q)
