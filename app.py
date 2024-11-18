d583f5ed7b60ad813fac3e5cb076ed456758bc5f
FroyaTheHen <kingacepielik@gmail.com>
2024-11-18 13:56:27 -0600

    feat: add django app

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from cassandra.auth import PlainTextAuthProvider
 
 
 auth_provider = PlainTextAuthProvider(username="cassandra", password="cassandra")
-cluster = Cluster(auth_provider=auth_provider, protocol_version=3, contact_points=['0.0.0.0'], port=9042)
+cluster = Cluster(auth_provider=auth_provider, protocol_version=3, contact_points=['172.21.0.3'], port=9042)
 session = cluster.connect()
 q = """CREATE KEYSPACE IF NOT EXISTS xela_keyspace
 WITH replication = {
